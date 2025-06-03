513583602ec818b10d154dc3d69441e903c4ce74
Bakunga Bronson <bakungabro@gmail.com>
2025-06-03 23:03:12 +0800

    Repo clean-up

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: {
-    // Fixme (Nour): enabling allows RTL display, but smoehow removes the sidebar... Needs more investigation
-    // 'postcss-rtlcss': { /* plugin options */ },
-  },
-}
